c2bc8c1ee6210620d4a828b1b98c9e1e3ceb8818
Alex Guaman <alex.guaman86@gmail.com>
2021-11-24 22:17:25 -0500

    Cambios para github pages

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-		<BrowserRouter>
+		<BrowserRouter basename="/platzi-react-shop">
 			<Layout>
 				<Switch>
 					<Route exact path="/" component={Home} />
